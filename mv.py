3f40b028128be9b875783777e55a97c836887659
Kieran Hogg <kieran.hogg@gmail.com>
2023-12-10 21:20:03 +0000

    .

--- mv.py
+++ mv.py
@@ -10,32 +10,44 @@ import matplotlib.pyplot as plt
 # You should drop any null values
 # You should drop PassengerId, Name, SibSp, Ticket, Cabin, Embarked, Sex columns
 
-titanic = pd.read_csv('titanic.csv')
-titanic = titanic.drop(columns=["PassengerId", "Name", "SibSp", "Ticket", "Cabin", "Embarked", "Sex"])
-titanic = titanic.drop_duplicates()
-titanic = titanic.dropna()
-
+df = pd.read_csv('titanic.csv')
+columns__to_drop = ["PassengerId", "Name", "SibSp", "Ticket", "Cabin", "Embarked", "Sex"]
+df = df.drop(columns=columns__to_drop)
+df = df.drop_duplicates()
+df = df.dropna(how='any')
 
 # You will now build a simple Logistic Regression model using the sklearn library
 # You should use the following columns as your features Pclass, Age, Parch, Fare for your X values
 # The Y value should be the survived column
 
-survived = titanic.pop("Survived")
-X_train, X_test, y_train, y_test = train_test_split(titanic, survived)
+survived = df.pop("Survived")
+X_train, X_test, y_train, y_test  = train_test_split(df, survived)
 model = LogisticRegression().fit(X=X_train, y=y_train)
+
+# Check the score of your model for the train values
+print(model.score(X=X_train, y=y_train))
+
+# Check the score of your model for the test values
+print(model.score(X=X_test, y=y_test))
+
+# Produce a classification report for your y values
 report = classification_report(y_train, model.predict(X_train))
+
+# Produce a confusion matrix for your model
 c_matrix = confusion_matrix(y_test, model.predict(X_test))
-# probs = model.predict_proba(X_test)
-# preds = probs[:,1]
-fpr, tpr, thresholds = roc_curve(y_test, model.predict_proba(X_test)[:,1])
+print(f"TN: {c_matrix[0][0]} FN: {c_matrix[0][1]} TP: {c_matrix[1][0]} FP: {c_matrix[1][1]}")
+
+# Plot a ROC curve for your model
+y_scores = model.predict_proba(X_test)
+y_scores = model.predict_proba(X_test)[:,1]
+fpr, tpr, thresholds = roc_curve(y_test, y_scores)
 plt.plot(fpr, tpr)
+plt.xlabel("FPR")
+plt.ylabel("TPR")
 plt.show()
 
 
 
-# Check the score of your model for the train values
-# Check the score of your model for the test values
-# Produce a classification report for your y values
 
-# Produce a confusion matrix for your model
-# Plot a ROC curve for your model
+
+
